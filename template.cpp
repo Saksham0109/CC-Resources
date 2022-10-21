8d387091937a3782cad8729126f0d58f3b1e696c
Saksham0109 <f20212414@pilani.bits-pilani.ac.in>
2022-10-21 17:31:06 +0530

    Template for cpp users

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forn(i,a,n) for(long long i = a; i < n; i++)
+#define endl "\n" 
+#define int long long
+
+const int MAXN = 200100;
+const int MOD = 1000000007;
+
+void solve() {
+    //Write you code here
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int T = 1;
+    cin >> T;//Comment this out in case number of test cases are not to be taken as input.
+    for(int I = 1; I <= T; I++) {
+        solve(); 
+    }
+    return 0;
+}
